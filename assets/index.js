dbc42f1dd1015193add733f74a7482bfd6a4b17b
Drabis <drabis47@gmail.com>
2021-06-11 13:11:43 -0400

    cchanged the api key

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,13 @@ var searchFormEl = document.querySelector("#search-form");
 var teamEl = document.querySelector("#team-dropdown");
 
 // Function to set localStorage
-document.getElementById("team-dropdown").onchange = function() {
-    localStorage["userChoice"] = document.getElementById("team-dropdown").value;
-}
-window.onload= function(){
-    if(localStorage["userChoice"])
+document.getElementById("team-dropdown").onchange = function () {
+  localStorage["userChoice"] = document.getElementById("team-dropdown").value;
+};
+window.onload = function () {
+  if (localStorage["userChoice"])
     document.getElementById("team-dropdown").value = localStorage["userChoice"];
-}
+};
 
 // Function to handle submit button
 function handleSearchFormSubmit(event) {
@@ -29,34 +29,37 @@ function handleSearchFormSubmit(event) {
 searchFormEl.addEventListener("submit", handleSearchFormSubmit);
 
 // Fetch that gets 2020-2021 season team information
-fetch("https://api-basketball.p.rapidapi.com/teams?league=12&season=2020-2021", {
-  "method": "GET",
-	"headers": {
-		"x-rapidapi-key": "a02badb577msh53c62c24f7e4112p157b9bjsne248c865880a",
-		"x-rapidapi-host": "api-basketball.p.rapidapi.com"
-	}
-})
-.then(response => {
-	if (response.ok) {
-    response.json().then(function(data) {
-      console.log(data);
-      displayTeamData(data);
-    });
+fetch(
+  "https://api-basketball.p.rapidapi.com/teams?league=12&season=2020-2021",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "a6c2bc5e36msh36a0b4534b1f40dp1877cejsn8d86f8e13484",
+      "x-rapidapi-host": "api-basketball.p.rapidapi.com",
+    },
   }
-})
-.catch(err => {
-	console.error(err);
-});
+)
+  .then((response) => {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayTeamData(data);
+      });
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // Function with for loop which populates team names in drop down menu
 function displayTeamData(data) {
-  for (var i = 0; i < data.response.length-2; i++) {
+  for (var i = 0; i < data.response.length - 2; i++) {
     var teamName = data.response[i].name;
     console.log(teamName);
     console.log(data.response[i].id);
     var teamTitle = document.createElement("option");
     teamTitle.textContent = teamName;
     teamTitle.value = teamName.toLowerCase();
-    teamEl.appendChild(teamTitle);  
-  }   
+    teamEl.appendChild(teamTitle);
+  }
 }
--- assets/results.js
+++ assets/results.js
@@ -12,195 +12,206 @@ var teamPointsAgainstEl = document.querySelector("#teamPointsAgainst");
 
 // If no team is selected accidentally, page sends user back to index.html
 if (!team) {
-    location.replace("./index.html");
+  location.replace("./index.html");
 }
 
 // Object with all teams and their ids
 var teamIds = {
-    atlantahawks: 132,
-    bostonceltics: 133,
-    brooklynnets: 134,
-    charlottehornets: 135,
-    chicagobulls: 136,
-    clevelandcavaliers: 137,
-    dallasmavericks: 138,
-    denvernuggets: 139,
-    detroitpistons: 140,
-    goldenstatewarriors: 141,
-    houstonrockets: 142,
-    indianapacers: 143,
-    losangelesclippers: 144,
-    losangeleslakers: 145,
-    memphisgrizzlies: 146,
-    miamiheat: 147,
-    milwaukeebucks: 148,
-    minnesotatimberwolves: 149,
-    neworleanspelicans: 150,
-    newyorkknicks: 151,
-    oklahomacitythunder: 152,
-    orlandomagic: 153,
-    philadelphia76ers: 154,
-    phoenixsuns: 155,
-    portlandtrailblazers: 156,
-    sacramentokings: 157,
-    sanantoniospurs: 158,
-    torontoraptors: 159,
-    utahjazz: 160,
-    washingtonwizards: 161
-}
+  atlantahawks: 132,
+  bostonceltics: 133,
+  brooklynnets: 134,
+  charlottehornets: 135,
+  chicagobulls: 136,
+  clevelandcavaliers: 137,
+  dallasmavericks: 138,
+  denvernuggets: 139,
+  detroitpistons: 140,
+  goldenstatewarriors: 141,
+  houstonrockets: 142,
+  indianapacers: 143,
+  losangelesclippers: 144,
+  losangeleslakers: 145,
+  memphisgrizzlies: 146,
+  miamiheat: 147,
+  milwaukeebucks: 148,
+  minnesotatimberwolves: 149,
+  neworleanspelicans: 150,
+  newyorkknicks: 151,
+  oklahomacitythunder: 152,
+  orlandomagic: 153,
+  philadelphia76ers: 154,
+  phoenixsuns: 155,
+  portlandtrailblazers: 156,
+  sacramentokings: 157,
+  sanantoniospurs: 158,
+  torontoraptors: 159,
+  utahjazz: 160,
+  washingtonwizards: 161,
+};
 
 // Function to pull locally saved team logos
 function getLogo(team) {
-    return  "./assets/teamlogos/" + team + ".svg";
+  return "./assets/teamlogos/" + team + ".svg";
 }
 
 function getStats(teamID) {
-    fetch("https://api-basketball.p.rapidapi.com/statistics?league=12&season=2020-2021&team=" + teamID, {
-	    "method": "GET",
-	    "headers": {
-            "x-rapidapi-key": "a02badb577msh53c62c24f7e4112p157b9bjsne248c865880a",
-            "x-rapidapi-host": "api-basketball.p.rapidapi.com"
-        }
+  fetch(
+    "https://api-basketball.p.rapidapi.com/statistics?league=12&season=2020-2021&team=" +
+      teamID,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "a6c2bc5e36msh36a0b4534b1f40dp1877cejsn8d86f8e13484",
+        "x-rapidapi-host": "api-basketball.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          displayTeamData(data);
+        });
+      }
     })
-    .then(response => {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                displayTeamData(data);
-            });
-        }
-    })
-    .catch(err => {
-        console.error(err);
+    .catch((err) => {
+      console.error(err);
     });
 }
 
-    // Function displays team data - name, logo, etc
-    function displayTeamData(data) {
+// Function displays team data - name, logo, etc
+function displayTeamData(data) {
+  // TEAM NAMES
+  var teamName = data.response.team.name;
 
-        // TEAM NAMES
-        var teamName = data.response.team.name;
+  // Creates span for titles with team names, the append to HTML EL
+  var teamTitle = document.createElement("h2");
+  teamTitle.textContent = teamName;
+  teamTitle.setAttribute(
+    "style",
+    "background-color: #000; font-family: 'Anton', sans-serif; font-size: var(--title-font);"
+  );
+  teamEl.appendChild(teamTitle);
 
-        // Creates span for titles with team names, the append to HTML EL
-        var teamTitle = document.createElement('h2');
-        teamTitle.textContent = teamName;
-        teamTitle.setAttribute("style", "background-color: #000; font-family: 'Anton', sans-serif; font-size: var(--title-font);");
-        teamEl.appendChild(teamTitle);
+  // TEAM LOGOS
+  var teamLogo = getLogo(teamName.toLowerCase().replaceAll(" ", ""));
+  var logo = document.createElement("img");
+  logo.setAttribute("src", teamLogo);
+  logo.setAttribute("height", "100%");
+  logo.setAttribute("width", "450px");
+  logo.setAttribute("alt", "Team Logo");
+  document.getElementById("selectedTeam").appendChild(logo);
 
-        // TEAM LOGOS
-        var teamLogo = getLogo( teamName.toLowerCase().replaceAll(' ', '') );
-        var logo = document.createElement("img");
-        logo.setAttribute("src", teamLogo);
-        logo.setAttribute("height", "100%");
-        logo.setAttribute("width", "450px");
-        logo.setAttribute("alt", "Team Logo");
-        document.getElementById("selectedTeam").appendChild(logo);
+  // TEAM STATS TABLE
 
-        // TEAM STATS TABLE
+  // Team wins
+  var teamWins = data.response.games.wins.all.total;
+  teamWinsEl.textContent = teamWins;
 
-        // Team wins
-        var teamWins = data.response.games.wins.all.total;
-        teamWinsEl.textContent = teamWins;
+  // Team losses
+  var teamLosses = data.response.games.loses.all.total;
+  teamLossesEl.textContent = teamLosses;
 
-        // Team losses
-        var teamLosses = data.response.games.loses.all.total;
-        teamLossesEl.textContent = teamLosses;
+  // Team points for
+  var teamPointsFor = data.response.goals.for.average.all;
+  teamPointsForEl.textContent = teamPointsFor;
 
-        // Team points for
-        var teamPointsFor = data.response.goals.for.average.all;
-        teamPointsForEl.textContent = teamPointsFor;
-
-        // Team points against
-        var teamPointsAgainst = data.response.goals.against.average.all;
-        teamPointsAgainstEl.textContent = teamPointsAgainst;
-    }
-  
-    // GAME TIME
-    function gameCountdown(data) {
-
-        // Displays when the game will take place
-        // var time = moment(data[0].dates.start.localDate).format("dddd, MMMM Do h:mm a");
-        // gameTimeEl.textContent = time;
-
-        // Countdown from now till game
-        var countDownDate = new Date(data[0].dates.start.localDate).getTime();
-
-        var x = setInterval(function() {
-            var now = new Date().getTime();
-            var distance = countDownDate - now;
-
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-                + minutes + "m " + seconds + "s ";
-        }, 1000);
-    };
-    
-    function getTeamInfo() {
-        var teamCode = team.toLowerCase().replaceAll('%20', '');
-        var logo = getLogo(teamCode);
-        var teamId = teamIds[teamCode];
-        var winLoss = getStats(teamId);
-        console.log(teamCode);
-    }
+  // Team points against
+  var teamPointsAgainst = data.response.goals.against.average.all;
+  teamPointsAgainstEl.textContent = teamPointsAgainst;
+}
 
 // GAME TIME
 function gameCountdown(data) {
+  // Displays when the game will take place
+  // var time = moment(data[0].dates.start.localDate).format("dddd, MMMM Do h:mm a");
+  // gameTimeEl.textContent = time;
 
-    // Countdown till game
-    var countDownDate = new Date(data[0].dates.start.localDate).getTime();
+  // Countdown from now till game
+  var countDownDate = new Date(data[0].dates.start.localDate).getTime();
 
-    var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-    }, 1000);
-};
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }, 1000);
+}
 
 function getTeamInfo() {
-    var teamCode = team.toLowerCase().replaceAll("%20", "");
-    var logo = getLogo(teamCode);
-    var teamId = teamIds[teamCode];
-    var winLoss = getStats(teamId);
-    console.log(teamCode);
+  var teamCode = team.toLowerCase().replaceAll("%20", "");
+  var logo = getLogo(teamCode);
+  var teamId = teamIds[teamCode];
+  var winLoss = getStats(teamId);
+  console.log(teamCode);
+}
+
+// GAME TIME
+function gameCountdown(data) {
+  // Countdown till game
+  var countDownDate = new Date(data[0].dates.start.localDate).getTime();
+
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }, 1000);
+}
+
+function getTeamInfo() {
+  var teamCode = team.toLowerCase().replaceAll("%20", "");
+  var logo = getLogo(teamCode);
+  var teamId = teamIds[teamCode];
+  var winLoss = getStats(teamId);
+  console.log(teamCode);
 }
 
 getTeamInfo();
 
 // Ticketmaster API
 $.ajax({
-    type: "GET",
-    url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + team + "&apikey=wkeUKU7nGwu8KaAr6CW0pqhy3LP5gh4f",
-    async: true,
-    dataType: "json",
-    success: function (res) {
-        console.log("**********",res);
-        const eventData = res._embedded.events;
+  type: "GET",
+  url:
+    "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" +
+    team +
+    "&apikey=wkeUKU7nGwu8KaAr6CW0pqhy3LP5gh4f",
+  async: true,
+  dataType: "json",
+  success: function (res) {
+    console.log("**********", res);
+    const eventData = res._embedded.events;
 
-        gameCountdown(eventData);
+    gameCountdown(eventData);
 
-        //For loop to creata future game cards
-        for (var i = 0; i < eventData.length; i++) {
-            var teams = eventData[i].name.split(' vs. ');
-            for (var j = 0; j < teams.length; j++ ) {
-                teams[j] = teams[j].toLowerCase().replaceAll(' ', '');
-            };
-            console.log(teams);
-            var team1Logo = getLogo(teams[0]);
-            var team2Logo = getLogo(teams[1]);
-            gameCardsEl.innerHTML += `
+    //For loop to creata future game cards
+    for (var i = 0; i < eventData.length; i++) {
+      var teams = eventData[i].name.split(" vs. ");
+      for (var j = 0; j < teams.length; j++) {
+        teams[j] = teams[j].toLowerCase().replaceAll(" ", "");
+      }
+      console.log(teams);
+      var team1Logo = getLogo(teams[0]);
+      var team2Logo = getLogo(teams[1]);
+      gameCardsEl.innerHTML += `
                 <div class="card row nba-card">
                     <h3 class="nba-title">${eventData[i].name}</h3>
                     <h5>Next Game:</h5>
@@ -216,8 +227,7 @@ $.ajax({
                     <a class="button alert" href="${eventData[i].url}" target="_blank">Click here to visit Ticket Master</a>
                 </div>
             `;
-        };
-    },
-    error: function (xhr, status, err) {
     }
+  },
+  error: function (xhr, status, err) {},
 });
